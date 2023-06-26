5f27d25749d45ea2cefb8ef3afc9babdc1f15353
headkaze <headkaze@gmail.com>
2023-06-26 13:35:29 -0700

    Use ACSOPATCH_VER variable

--- build-debian.sh
+++ build-debian.sh
@@ -3,7 +3,7 @@ ACSOPATCH_VER=6.3
 KERNEL_VER=6.3
 sudo apt install build-essential libncurses5-dev fakeroot xz-utils libelf-dev liblz4-tool \
   unzip flex bison bc debhelper rsync libssl-dev:native
-wget -N https://raw.githubusercontent.com/benbaker76/linux-acs-override/main/6.3/acso.patch
+wget -N https://raw.githubusercontent.com/benbaker76/linux-acs-override/main/$ACSOPATCH_VER/acso.patch
 wget -N https://github.com/torvalds/linux/archive/refs/tags/v$KERNEL_VER.zip
 unzip -o v$KERNEL_VER.zip
 cd linux-$KERNEL_VER
