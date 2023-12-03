62a3485a8326043169a752d0c91ee70652b77002
Zach Bloomquist <github@chary.us>
2023-12-02 20:47:47 -0500

    deps: add dwarves to build-debian.sh

--- build-debian.sh
+++ build-debian.sh
@@ -2,7 +2,7 @@
 ACSOPATCH_VER=6.3
 KERNEL_VER=6.3
 sudo apt install build-essential libncurses5-dev fakeroot xz-utils libelf-dev liblz4-tool \
-  unzip flex bison bc debhelper rsync libssl-dev:native
+  unzip flex bison bc debhelper rsync libssl-dev:native dwarves
 wget -N https://raw.githubusercontent.com/benbaker76/linux-acs-override/main/$ACSOPATCH_VER/acso.patch
 wget -N https://github.com/torvalds/linux/archive/refs/tags/v$KERNEL_VER.zip
 unzip -o v$KERNEL_VER.zip
